382289788078cfd0115a3aaf49db09b32be8464c
john-kanel <john@templarsorder.org>
2025-06-27 21:08:26 -0500

    Add comprehensive favicon support
    - Added favicon.ico (multi-size ICO file)
    - Added PNG favicons for 16x16, 32x32, 192x192, 512x512
    - Added apple-touch-icon.png for iOS devices
    - Updated layout.tsx with proper favicon meta tags
    - Full cross-platform favicon support for browsers and mobile devices

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,14 @@ export default function RootLayout({
         <link rel="preload" href="/fonts/Montserrat-Medium.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
         <link rel="preload" href="/fonts/Montserrat-SemiBold.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
         <link rel="preload" href="/fonts/Montserrat-Bold.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+        
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
       </head>
       <body suppressHydrationWarning={true}>
         <SessionProvider>
